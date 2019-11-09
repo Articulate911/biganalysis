07779c028661ec50f281bc01df1c9bd6a6b039ef
JinYijie <jinyijie@stu.hit.edu.cn>
2019-11-10 00:17:14 +0800

    11/10 midnight

--- government/models.py
+++ government/models.py
@@ -10,36 +10,36 @@ from django.db import models
 
 class PeopleRequest(models.Model):
     id = models.IntegerField(db_column='id', primary_key=True)
-    report_num = models.TextField(db_column='REPORT_NUM', blank=True, null=True)  # Field name made lowercase.
-    event_property_name = models.TextField(db_column='EVENT_PROPERTY_NAME', blank=True, null=True)  # Field name made lowercase.
-    event_type_id = models.TextField(db_column='EVENT_TYPE_ID', blank=True, null=True)  # Field name made lowercase.
-    event_type_name = models.TextField(db_column='EVENT_TYPE_NAME', blank=True, null=True)  # Field name made lowercase.
-    event_src_name = models.TextField(db_column='EVENT_SRC_NAME', blank=True, null=True)  # Field name made lowercase.
-    district_id = models.TextField(db_column='DISTRICT_ID', blank=True, null=True)  # Field name made lowercase.
-    intime_archive_num = models.TextField(db_column='INTIME_ARCHIVE_NUM', blank=True, null=True)  # Field name made lowercase.
-    sub_type_id = models.TextField(db_column='SUB_TYPE_ID', blank=True, null=True)  # Field name made lowercase.
-    district_name = models.TextField(db_column='DISTRICT_NAME', blank=True, null=True)  # Field name made lowercase.
-    community_id = models.TextField(db_column='COMMUNITY_ID', blank=True, null=True)  # Field name made lowercase.
-    rec_id = models.TextField(db_column='REC_ID', blank=True, null=True)  # Field name made lowercase.
-    street_id = models.TextField(db_column='STREET_ID', blank=True, null=True)  # Field name made lowercase.
-    overtime_archive_num = models.TextField(db_column='OVERTIME_ARCHIVE_NUM', blank=True, null=True)  # Field name made lowercase.
-    operate_num = models.TextField(db_column='OPERATE_NUM', blank=True, null=True)  # Field name made lowercase.
-    dispose_unit_id = models.TextField(db_column='DISPOSE_UNIT_ID', blank=True, null=True)  # Field name made lowercase.
-    street_name = models.TextField(db_column='STREET_NAME', blank=True, null=True)  # Field name made lowercase.
-    create_time = models.TextField(db_column='CREATE_TIME', blank=True, null=True)  # Field name made lowercase.
-    event_src_id = models.TextField(db_column='EVENT_SRC_ID', blank=True, null=True)  # Field name made lowercase.
-    intime_to_archive_num = models.TextField(db_column='INTIME_TO_ARCHIVE_NUM', blank=True, null=True)  # Field name made lowercase.
-    sub_type_name = models.TextField(db_column='SUB_TYPE_NAME', blank=True, null=True)  # Field name made lowercase.
-    event_property_id = models.TextField(db_column='EVENT_PROPERTY_ID', blank=True, null=True)  # Field name made lowercase.
-    occur_place = models.TextField(db_column='OCCUR_PLACE', blank=True, null=True)  # Field name made lowercase.
-    community_name = models.TextField(db_column='COMMUNITY_NAME', blank=True, null=True)  # Field name made lowercase.
-    dispose_unit_name = models.TextField(db_column='DISPOSE_UNIT_NAME', blank=True, null=True)  # Field name made lowercase.
-    main_type_name = models.TextField(db_column='MAIN_TYPE_NAME', blank=True, null=True)  # Field name made lowercase.
-    main_type_id = models.TextField(db_column='MAIN_TYPE_ID', blank=True, null=True)  # Field name made lowercase.
+    report_num = models.IntegerField(db_column='REPORT_NUM', null=True)
+    event_property_name = models.TextField(db_column='EVENT_PROPERTY_NAME', null=True)  
+    event_type_id = models.IntegerField(db_column='EVENT_TYPE_ID', null=True)
+    event_type_name = models.TextField(db_column='EVENT_TYPE_NAME', null=True)  
+    event_src_name = models.TextField(db_column='EVENT_SRC_NAME', null=True)  
+    district_id = models.IntegerField(db_column='DISTRICT_ID', null=True)
+    intime_archive_num = models.IntegerField(db_column='INTIME_ARCHIVE_NUM', null=True)
+    sub_type_id = models.IntegerField(db_column='SUB_TYPE_ID', null=True)  
+    district_name = models.TextField(db_column='DISTRICT_NAME', null=True)  
+    community_id = models.IntegerField(db_column='COMMUNITY_ID', null=True)  
+    rec_id = models.TextField(db_column='REC_ID', null=True)  
+    street_id = models.IntegerField(db_column='STREET_ID', null=True)  
+    overtime_archive_num = models.IntegerField(db_column='OVERTIME_ARCHIVE_NUM', null=True)  
+    operate_num = models.IntegerField(db_column='OPERATE_NUM', null=True)  
+    dispose_unit_id = models.IntegerField(db_column='DISPOSE_UNIT_ID', null=True)  
+    street_name = models.TextField(db_column='STREET_NAME', null=True)  
+    create_time = models.DateTimeField(db_column='CREATE_TIME', null=True)  
+    event_src_id = models.IntegerField(db_column='EVENT_SRC_ID', null=True)  
+    intime_to_archive_num = models.IntegerField(db_column='INTIME_TO_ARCHIVE_NUM', null=True)  
+    sub_type_name = models.TextField(db_column='SUB_TYPE_NAME', null=True)  
+    event_property_id = models.IntegerField(db_column='EVENT_PROPERTY_ID', null=True)  
+    occur_place = models.TextField(db_column='OCCUR_PLACE', null=True)  
+    community_name = models.TextField(db_column='COMMUNITY_NAME', null=True)  
+    dispose_unit_name = models.TextField(db_column='DISPOSE_UNIT_NAME', null=True)  
+    main_type_name = models.TextField(db_column='MAIN_TYPE_NAME', null=True)  
+    main_type_id = models.IntegerField(db_column='MAIN_TYPE_ID', null=True)  
 
     class Meta:
         db_table = '坪山区-民生诉求数据_完整版'
         ordering = ('id',)
 
     def __str__(self):
-        return self.create_time
+        return str(self.create_time)
