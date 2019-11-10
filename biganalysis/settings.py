08311596118f5d65f38cd71e2a4243126f8f7b9a
JinYijie <jinyijie@stu.hit.edu.cn>
2019-11-10 20:12:47 +0800

    11/10 20:12

--- biganalysis/settings.py
+++ biganalysis/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'biganalysis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,6 +71,12 @@ TEMPLATES = [
         },
     },
 ]
+
+# Add for vue.js
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/dist/static"),
+]
+
 
 WSGI_APPLICATION = 'biganalysis.wsgi.application'
 
--- biganalysis/urls.py
+++ biganalysis/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('government/', include('government.urls', namespace='government')),
+    path(r'', TemplateView.as_view(template_name="vuehello.html")),
 ]
