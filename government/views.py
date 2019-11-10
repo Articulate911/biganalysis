75851ad6a2be80d8d91840dfcbbc6aed48385ed8
JinYijie <jinyijie@stu.hit.edu.cn>
2019-11-10 15:23:03 +0800

    11/10 15:22

--- government/views.py
+++ government/views.py
@@ -1,6 +1,12 @@
 # 导入 HttpResponse 模块
 from django.http import HttpResponse
+from .models import PeopleRequest
+from django.shortcuts import render
 
 # 视图函数
 def request_list(request):
-    return HttpResponse("Hello World!")
+    requests = PeopleRequest.objects.all()
+    # 需要传递给模板（templates）的对象
+    context = { 'requests': requests }
+    # render函数：载入模板，并返回context对象
+    return render(request, 'government/list.html', context)
